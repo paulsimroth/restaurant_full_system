4507d81a08a9db6bf3033ac077ac1c39400c9996
Paul Simroth <paul.simroth@proton.me>
2023-06-26 18:26:05 +0200

    imports

---
+++ src/lib/nodemailer.js
@@ -0,0 +1,18 @@
+import nodemailer from "nodemailer";
+
+// GMAIL VERSION
+const email = process.env.CONTACTFORM_EMAIL;
+const password = process.env.CONTACTFORM_EMAIL_PASSWORD;
+
+export const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: email,
+        pass: password,
+    },
+});
+
+export const mailOptions = {
+    from: email,
+    to: email,
+};
